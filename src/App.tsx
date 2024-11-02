adbb399b466c6fb07d10f629a155168e1b8c405a
ytraddan <1nightshelter@gmail.com>
2024-11-02 12:02:32 +0300

    refactor: extract generatePokemon function to reduce duplication

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,36 @@ import "./App.css";
 import RandomPokemon from "./RandomPokemon";
 import { useState, useEffect } from "react";
 
+const generatePokemon = () => {
+  const id = Math.floor(Math.random() * 151) + 1;
+  return {
+    id,
+    url: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+  };
+};
+
 export default function App() {
   const [pokemon, setPokemon] = useState(() =>
-    Array.from({ length: 3 }, (_, i) => ({
-      id: i + 1,
+    Array.from({ length: 3 }, () => ({
+      id: 0,
       url: "",
     }))
   );
 
   useEffect(() => {
-    setPokemon(() => 
-      Array.from({ length: 3 }, () => {
-        const id = Math.floor(Math.random() * 151) + 1;
-        return {
-          id,
-          url: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
-        };
-      })
-    );
+    setPokemon(() => Array.from({ length: 3 }, generatePokemon));
   }, []);
+
+  const handleClick = () => {
+    setPokemon(pokemon.map(() => generatePokemon()));
+  };
 
   return (
     <div className="App">
-      {pokemon.map((pokemon , i) => (
+      {pokemon.map((pokemon, i) => (
         <RandomPokemon key={i} id={pokemon.id} url={pokemon.url} />
       ))}
-      <button onClick={() => setPokemon(pokemon.map(() => ({
-        id: Math.floor(Math.random() * 151) + 1,
-        url: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${Math.floor(Math.random() * 151) + 1}.png`,
-      })))}>Refresh</button>
+      <button onClick={handleClick}>Refresh</button>
     </div>
   );
 }
