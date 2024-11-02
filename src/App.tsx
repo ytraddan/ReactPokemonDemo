c4f71ca4f9c19d47283c6e1d1d382a512efef62c
ytraddan <1nightshelter@gmail.com>
2024-11-02 11:37:51 +0300

    fix: remove unnecessary handleClick dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import RandomPokemon from "./RandomPokemon";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
   const [pokemon, setPokemon] = useState(() =>
@@ -10,7 +10,7 @@ export default function App() {
     }))
   );
 
-  function handleClick() {
+  useEffect(() => {
     setPokemon(
       pokemon.map(() => {
         const id = Math.floor(Math.random() * 151) + 1;
@@ -20,17 +20,17 @@ export default function App() {
         };
       })
     );
-  }
-
-  // Call once on mount
-  useState(() => handleClick());
+  }, []);
 
   return (
     <div className="App">
       {pokemon.map((pokemon , i) => (
         <RandomPokemon key={i} id={pokemon.id} url={pokemon.url} />
       ))}
-      <button onClick={handleClick}>Refresh</button>
+      <button onClick={() => setPokemon(pokemon.map(() => ({
+        id: Math.floor(Math.random() * 151) + 1,
+        url: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${Math.floor(Math.random() * 151) + 1}.png`,
+      })))}>Refresh</button>
     </div>
   );
 }
